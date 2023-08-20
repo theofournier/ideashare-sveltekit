4e69d43aecebcec2812ed687f07b13062d466147
theofournier <fournier.theo@gmail.com>
2023-08-20 12:40:52 -0400

    Add notes

--- src/routes/(app)/posts/[postId]/notes/+page.server.ts
+++ src/routes/(app)/posts/[postId]/notes/+page.server.ts
@@ -1,5 +1,60 @@
-import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+export const load: PageServerLoad = async ({ locals: { supabase }, params }) => {
+	const { data, error } = await supabase
+		.from('posts_notes')
+		.select('*')
+		.eq('post_id', params.postId)
+		.order('created_at', { ascending: false });
+
+	console.log('POST NOTES', error);
+	return { notes: data };
+};
+
+export const actions: Actions = {
+	create: async ({ request, locals: { supabase, getSession }, params }) => {
+		const session = await getSession();
+		if (!session) {
+			return fail(401, {
+				error: 'You must be logged in'
+			});
+		}
+		const formData = await request.formData();
+		const note = formData.get('note') as string;
+
+		if (!note) {
+			return fail(404, {
+				error: 'Note required'
+			});
+		}
+
+		const { error } = await supabase.from('posts_notes').insert({
+			post_id: params.postId,
+			user_id: session.user.id,
+			text: note
+		});
+
+		console.log('CREATE NOTE', error);
+	},
+	delete: async ({ request, locals: { supabase, getSession }, params }) => {
+		const session = await getSession();
+		if (!session) {
+			return fail(401, {
+				error: 'You must be logged in'
+			});
+		}
+
+		const formData = await request.formData();
+		const id = formData.get('id') as string;
+		if (!id) {
+			return fail(404, {
+				error: 'Id required'
+			});
+		}
+
+		const { error } = await supabase.from('posts_notes').delete().eq('id', id);
+
+		console.log('DELETE NOTE', error);
+	}
+};
--- src/routes/(app)/posts/[postId]/notes/+page.svelte
+++ src/routes/(app)/posts/[postId]/notes/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
+	export let data;
 </script>
 
-<p>Notes</p>
+<form method="POST" action="?/create">
+	<textarea placeholder="New note" class="textarea textarea-bordered" name="note" />
+	<button class="btn">Save</button>
+</form>
+{#if !data.notes || data.notes.length === 0}
+	<p>No notes</p>
+{:else}
+	{#each data.notes as note (note.id)}
+		<div class="border-solid border-2 border-black">
+			<p>{note.text}</p>
+			<p>{note.created_at}</p>
+			<form method="POST" action="?/delete">
+				<input type="hidden" name="id" value={note.id} />
+				<button class="btn">❌</button>
+			</form>
+		</div>
+	{/each}
+{/if}
