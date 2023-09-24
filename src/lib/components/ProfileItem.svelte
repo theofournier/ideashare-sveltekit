aec68d37cf2244ee9f6055432191ca1edfe78824
theofournier <fournier.theo@gmail.com>
2023-09-24 12:35:32 -0400

    Add profile id in item

--- src/lib/components/ProfileItem.svelte
+++ src/lib/components/ProfileItem.svelte
@@ -7,6 +7,7 @@
 <div class="card shadow-lg">
 	<div class="card-body">
 		<h2 class="card-title">{profile.first_name} {profile.last_name}</h2>
+		<p>{profile.id}</p>
 		<a href={`/profiles/${profile.id}`} class="btn">See profile</a>
 	</div>
 </div>
