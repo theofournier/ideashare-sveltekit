27803d013ca13283eb7bc1f0f50528dd5f6b8c30
theofournier <fournier.theo@gmail.com>
2023-10-29 13:00:25 -0400

    Add default share options

--- src/routes/(app)/settings/default-share-options/+page.server.ts
+++ src/routes/(app)/settings/default-share-options/+page.server.ts
@@ -1,5 +1,83 @@
-import type { PageServerLoad } from './$types';
+import { error, fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import type { ShareType } from '$lib/data/post';
+import type { DefaultShareOptionsUpdate } from '$lib/data/users';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
+	const session = await getSession();
+	if (!session) {
+		throw error(401, 'You must be logged in');
+	}
+	const { data: defaultShareOptions, error: err } = await supabase
+		.from('profiles_default_share_options')
+		.select('*')
+		.eq('user_id', session.user.id)
+		.single();
+	console.log('DEFAULT_SHARE_OPTIONS', err);
+
+	return { defaultShareOptions };
+};
+
+export const actions: Actions = {
+	save: async ({ request, locals: { supabase, getSession } }) => {
+		const session = await getSession();
+		if (!session) {
+			return fail(401, {
+				error: 'You must be logged in'
+			});
+		}
+
+		const formData = await request.formData();
+
+		const data: DefaultShareOptionsUpdate = {
+			privacy: formData.get('private')?.toString() === 'on' ? 'private' : 'public',
+			anonymous: formData.get('anonymous')?.toString() === 'on',
+			like: formData.get('like')?.toString() as ShareType,
+			comment: formData.get('comment')?.toString() as ShareType,
+			help: formData.get('help')?.toString() as ShareType,
+			link_post: formData.get('linkPost')?.toString() as ShareType,
+			work: formData.get('work')?.toString() as ShareType,
+			contact: formData.get('contact')?.toString() as ShareType,
+			follow: formData.get('follow')?.toString() as ShareType,
+			status: formData.get('status')?.toString() as ShareType
+		};
+
+		const { data: defaultShareOptions } = await supabase
+			.from('profiles_default_share_options')
+			.select('*')
+			.eq('user_id', session.user.id)
+			.single();
+
+		if (defaultShareOptions) {
+			const { error } = await supabase
+				.from('profiles_default_share_options')
+				.update(data)
+				.eq('user_id', session.user.id);
+
+			console.log('UPDATE DEFAULT SHARE OPTIONS', error);
+		} else {
+			const { error } = await supabase
+				.from('profiles_default_share_options')
+				.insert({ user_id: session.user.id, ...data });
+			console.log('INSERT DEFAULT SHARE OPTIONS', error);
+		}
+	},
+    reset: async ({ locals: { supabase, getSession } }) => {
+		const session = await getSession();
+		if (!session) {
+			return fail(401, {
+				error: 'You must be logged in'
+			});
+		}
+
+        const { error: errorDelete } = await supabase
+            .from('profiles_default_share_options')
+            .delete()
+            .eq("user_id", session.user.id);
+        console.log('DELETE DEFAULT SHARE OPTIONS', errorDelete);
+        const { error } = await supabase
+            .from('profiles_default_share_options')
+            .insert({ user_id: session.user.id });
+        console.log('INSERT DEFAULT SHARE OPTIONS', error);
+	}
+};
--- src/routes/(app)/settings/default-share-options/+page.svelte
+++ src/routes/(app)/settings/default-share-options/+page.svelte
@@ -1,1 +1,26 @@
-<div>Default Share Options</div>
+<script lang="ts">
+	import ShareOptionsSection from '$lib/components/post/form/ShareOptionsSection.svelte';
+
+	export let data;
+</script>
+
+<div>
+	<form method="POST" action="?/reset">
+		<button type="submit" class="btn">Reset to default</button>
+	</form>
+	<form method="POST" action="?/save">
+		<ShareOptionsSection
+			privacy={data.defaultShareOptions?.privacy}
+			anonymous={data.defaultShareOptions?.anonymous}
+			like={data.defaultShareOptions?.like}
+			comment={data.defaultShareOptions?.comment}
+			help={data.defaultShareOptions?.help}
+			link_post={data.defaultShareOptions?.link_post}
+			work={data.defaultShareOptions?.work}
+			contact={data.defaultShareOptions?.contact}
+			follow={data.defaultShareOptions?.follow}
+			status={data.defaultShareOptions?.status}
+		/>
+		<button class="btn">Save</button>
+	</form>
+</div>
