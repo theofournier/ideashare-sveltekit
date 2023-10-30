a81b6c7424ac4e9be9e97d02fed239fc4500316a
theofournier <fournier.theo@gmail.com>
2023-10-29 21:22:37 -0400

    Design form labels

--- src/lib/components/post/form/LabelsSection.svelte
+++ src/lib/components/post/form/LabelsSection.svelte
@@ -5,19 +5,17 @@
 	export let postLabels: PostLabel[] = [];
 </script>
 
-<div id="labels">
+<ul id="labels" class="list space-y-2">
 	{#each labels as label (label.value)}
-		<div class="form-control">
-			<label class="label cursor-pointer">
-				<span class="label-text">{label.name}</span>
-				<input
-					type="checkbox"
-					class="checkbox"
-					name="label"
-					value={label.value}
-					checked={postLabels.some((postLabel) => label.value === postLabel.label_id)}
-				/>
-			</label>
-		</div>
+		<label class="cursor-pointer flex items-center space-x-2 list-option">
+			<input
+				type="checkbox"
+				class="checkbox"
+				name="label"
+				value={label.value}
+				checked={postLabels.some((postLabel) => label.value === postLabel.label_id)}
+			/>
+			<span>{label.name}</span>
+		</label>
 	{/each}
-</div>
+</ul>
