7e6d1d10428a7c236b229daa80853850bee34dd0
theofournier <fournier.theo@gmail.com>
2023-11-02 20:48:12 -0400

    Remove skeleton and add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,13 @@
-import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
-import { skeleton } from '@skeletonlabs/tw-plugin';
 
 const config = {
 	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		forms,
-		skeleton({
-			themes: { preset: ['skeleton'] }
-		})
-	]
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	plugins: [forms, require('daisyui')],
+	daisyui: {
+		themes: ['retro']
+	}
 } satisfies Config;
 
 export default config;
